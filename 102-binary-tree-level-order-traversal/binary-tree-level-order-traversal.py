db00a1b2224217f2285f94355ee16589191f7469
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-05 16:09:01 +0300

    Time: 0 ms (100.00%) | Memory: 18.5 MB (39.04%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -9,17 +9,15 @@ class Solution:
         if not root:
             return []
         res = []
-        def bfs():
-            queue = deque([root])
-            while queue:
-                level = []
-                for _ in range(len(queue)):
-                    node = queue.popleft()
-                    level.append(node.val)
-                    if node.left:
-                        queue.append(node.left)
-                    if node.right:
-                        queue.append(node.right)
-                res.append(level)
-        bfs()
+        queue = deque([root])
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
         return res
