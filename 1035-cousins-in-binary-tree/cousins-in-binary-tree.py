b929190963cab2f7740e4f2f5b0c2550a173a608
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-11 18:41:10 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.51%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -11,25 +11,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        def f(node, val, depth):
-            if not node:
-                return None
-            if node.left and node.left.val == val:
-                return (depth, node)
-            if node.right and node.right.val == val:
-                return (depth, node)
-            return f(node.left, val, depth + 1) or f(node.right, val, depth + 1)
-        ans1 = f(root, x, 0) 
-        ans2 = f(root, y, 0)
-        if ans1 is None or ans2 is None:
-            return False
-        h1, p1 = ans1
-        h2, p2 = ans2    
-        print(h1, p1)
-        print(h2, p2)
+        
+        queue = deque([root])
+        d = dict()
+        depth = 0
+        d1 = dict()
+        prev = root
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                prev = node
+                d[node.val] = depth
+                if node.left:
+                    d1[node.left.val] = prev
+                    queue.append(node.left)
+                if node.right:
+                    d1[node.right.val] = prev
+                    queue.append(node.right)
+            if x in d and y in d and d[x] == d[y] and d1[x] != d1[y]:
+                return True
+        return False
 
-        return h1 == h2 and p1 != p2
+        # def f(node, val, depth):
+        #     if not node:
+        #         return (None, None)
+        #     if node.left and node.left.val == val:
+        #         return (depth, node)
+        #     if node.right and node.right.val == val:
+        #         return (depth, node)
+        #     return f(node.left, val, depth + 1) or f(node.right, val, depth + 1)
+        # h1, p1 = f(root, x, 0)
+        # h2, p2 = f(root, y, 0)
+        
+        # return h1 == h2 and p1 != p2
+        
 # @lc code=end
 
