e0e0a2136f6f0d6ccf0b5d38613c47b87e103377
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-12 15:14:58 +0300

    Time: 3 ms (28.86%) | Memory: 19.1 MB (24.04%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -15,21 +15,47 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
-        if not root.left and not root.right:
-            return root.val == targetSum
-        yes = False
-        def dfs(node, sm, depth):
-            nonlocal yes
-            if not node:
-                return 0
-            sm += node.val
-            if not node.left and not node.right:
-                if sm == targetSum and depth >= 1:
-                    yes = True
-                
-            dfs(node.left, sm, depth + 1)
-            dfs(node.right, sm, depth + 1)
-        dfs(root, 0, 0)
-        return yes
+        stack = []
+        current = root
+        prev_val = None
+        while stack or current:
+            if current:
+                if prev_val is not None:
+                    stack.append((current, current.val + prev_val))
+                    prev_val = None
+                elif len(stack) > 0:
+                    stack.append((current, current.val + stack[-1][1]))
+                else:
+                    stack.append((current, current.val))
+                current = current.left
+            else:
+                node, val = stack.pop()
+                prev_val = val
+                if node.val == 2:
+                    print(val)
+                if not node.right and not node.left:
+                    if val == targetSum:
+                        print(node)
+                        return True
+                current = node.right
+        return False
+
+        # if not root:
+        #     return False
+        # if not root.left and not root.right:
+        #     return root.val == targetSum
+        # yes = False
+        # def dfs(node, sm):
+        #     nonlocal yes
+        #     if not node:
+        #         return 0
+        #     sm += node.val
+        #     if not node.left and not node.right:
+        #         if sm == targetSum:
+        #             yes = True
+        #     dfs(node.left, sm)
+        #     dfs(node.right, sm)
+        # dfs(root, 0)
+        # return yes
 # @lc code=end
 
