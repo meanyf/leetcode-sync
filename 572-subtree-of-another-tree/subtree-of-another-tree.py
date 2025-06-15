31cce084ca55fca87c22c429610bc0e90bebca86
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-15 17:37:03 +0300

    Time: 55 ms (12.38%) | Memory: 17.9 MB (97.13%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -1,71 +1,49 @@
-#
-# @lc app=leetcode id=572 lang=python3
-#
-# [572] Subtree of Another Tree
-#
-
-# @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
+
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    def isSubtree(self, root: 'TreeNode', subRoot: 'TreeNode') -> bool:
         if not subRoot:
             return True
         if not root:
             return False
+        
+        def isSameTree(p: 'TreeNode', q: 'TreeNode') -> bool:
+            stack_p = [p]
+            stack_q = [q]
+            
+            while stack_p and stack_q:
+                node_p = stack_p.pop()
+                node_q = stack_q.pop()
+                
+                if not node_p and not node_q:
+                    continue
+                if not node_p or not node_q:
+                    return False
+                if node_p.val != node_q.val:
+                    return False
+                
+                stack_p.append(node_p.right)
+                stack_p.append(node_p.left)
+                stack_q.append(node_q.right)
+                stack_q.append(node_q.left)
+            
+            return not stack_p and not stack_q
+        
         stack = [root]
-        node_sub = None
-        node = None
-        res = deque()
+        
         while stack:
             node = stack.pop()
-            res.append(node)
-            if node:
+            if node.val == subRoot.val:
+                if isSameTree(node, subRoot):
+                    return True
+            if node.right:
                 stack.append(node.right)
+            if node.left:
                 stack.append(node.left)
-        stack = [root]
-        node_sub = subRoot
-        stack_sub = [subRoot]
-        while stack or res:
-            if stack_sub:
-                node_sub = stack_sub.pop()
-            
-            if stack:
-                node = stack.pop()
-            else:
-                if res:
-                    stack = [res[0]]
-                    res.popleft()
-            if node_sub and not node:
-                stack_sub = [subRoot]
-            if node:
-                if (
-    node_sub and node and
-    node.val == node_sub.val and
-    (not node.left and not node_sub.left or node.left and node_sub.left and node.left.val == node_sub.left.val) and
-    (not node.right and not node_sub.right or node.right and node_sub.right and node.right.val == node_sub.right.val)
-):
-                    if all(x is None for x in stack_sub) and not node.left and not node.right and not node_sub.left and not node_sub.right:
-                        return True
-                    stack_sub.append(node_sub.right)
-                    stack_sub.append(node_sub.left)
-                else:
-                    stack_sub = [subRoot]
-                    if res:
-                        stack = [res[0]]
-                        res.popleft()
-                    continue
-                stack.append(node.right)
-                stack.append(node.left)
+        
         return False
-        
-      
-        
-
-# @lc code=end
-
