880ed6ed47d72d1e068328c2ca7b012564b7b35e
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-17 10:19:25 +0300

    Time: 7 ms (85.73%) | Memory: 18.1 MB (22.15%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.py
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.py
@@ -1,43 +1,23 @@
-#
-# @lc app=leetcode id=1385 lang=python3
-#
-# [1385] Find the Distance Value Between Two Arrays
-#
-
-# @lc code=start
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
         arr2.sort()
         cnt = 0
-        for item in arr1:
-            print(cnt)
-            lo = 0
-            hi = len(arr2) - 1
+
+        for x in arr1:
+            lo, hi = 0, len(arr2) - 1
+            found = False
+
             while lo <= hi:
-                mi = (lo + hi) // 2
-                if arr2[mi] == item:
-                    if abs(item - arr2[mi] > d):
-                        cnt += 1
+                mid = (lo + hi) // 2
+                if abs(arr2[mid] - x) <= d:
+                    found = True
                     break
-                if arr2[mi] < item:
-                    lo = mi + 1
+                elif arr2[mid] < x:
+                    lo = mid + 1
                 else:
-                    hi = mi - 1
-            else:
-                if lo == 0:
-                    if abs(arr2[lo] - item) > d:
-                        cnt += 1
-                elif lo == len(arr2):
-                    if abs(arr2[lo - 1] - item) > d:
-                        cnt += 1
-                elif lo == len(arr2) - 1:
-                    if abs(arr2[lo] - item) > d and abs(arr2[lo - 1] - item) > d:
-                        cnt += 1      
-                elif 0 < lo < len(arr2) - 1:
-                    if abs(arr2[lo] - item) > d and abs(arr2[lo - 1] - item) > d and abs(arr2[lo + 1] - item) > d:
-                        cnt += 1
+                    hi = mid - 1
+
+            if not found:
+                cnt += 1
 
         return cnt
-        
-# @lc code=end
-
