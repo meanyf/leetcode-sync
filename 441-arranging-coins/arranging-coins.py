963f5cae649f3d400622e64c0207b4fd4f3a6aa4
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-19 14:04:35 +0300

    Time: 2 ms (69.45%) | Memory: 17.4 MB (99.43%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -7,12 +7,23 @@
 # @lc code=start
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        last = 0
-        cnt = 1
-        while n > last:
-            n -= cnt
-            last = cnt
-            cnt += 1
-        return last
+        lo = 0
+        hi = n 
+        while lo <= hi:
+            k = (lo + hi) // 2
+            ans = k * (k + 1) // 2
+            if ans <= n:
+                lo = k + 1
+            else:
+                hi = k - 1
+        return lo - 1
+        
+        # last = 0
+        # cnt = 1
+        # while n > last:
+        #     n -= cnt
+        #     last = cnt
+        #     cnt += 1
+        # return last
 # @lc code=end
 
