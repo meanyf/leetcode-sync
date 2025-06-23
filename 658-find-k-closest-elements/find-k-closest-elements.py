4f5003323fb46d50d17412c9fe213073d786233f
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-23 20:33:04 +0300

    Time: 19 ms (32.26%) | Memory: 19 MB (83.49%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,10 +1,4 @@
-#
-# @lc app=leetcode id=658 lang=python3
-#
-# [658] Find K Closest Elements
-#
-
-# @lc code=start
+from collections import deque
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         lo = 0
@@ -15,33 +9,25 @@ class Solution:
                 lo = mi + 1
             else:
                 hi = mi
-        print(lo)
         if lo == len(arr) or (lo == 0 and arr[lo] == x):
             if lo == 0:
                 return sorted(arr[:k])
             if lo == len(arr):
                 return sorted(arr[-k:])
-        
         l = lo
-        if arr[lo] != x:
-            if abs(x - arr[lo - 1]) <= abs(x - arr[lo]):
-                l = lo - 1
-            else:
-                l = lo
+        if arr[lo] != x and abs(x - arr[lo - 1]) <= abs(x - arr[lo]):
+            l -= 1
         r = l + 1
         cnt = 0
-        res = []
+        res = deque()
         while cnt < k:
             left = arr[l] if l > -1 else float('inf')
             right = arr[r] if r < len(arr) else float('inf')
             if abs(x - left) <= abs(x - right):
-                res.append(arr[l])
+                res.appendleft(arr[l])
                 l -= 1
             else:
                 res.append(arr[r])
                 r += 1
             cnt += 1
-        res.sort()
-        return res
-# @lc code=end
-
+        return list(res)
