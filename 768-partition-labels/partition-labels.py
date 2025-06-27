bd40c9094a107c06c835e327352246e4fd292ed8
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-27 10:37:57 +0300

    Time: 3 ms (91.26%) | Memory: 17.7 MB (83.28%) - LeetSync

---
+++ 768-partition-labels/partition-labels.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=763 lang=python3
+#
+# [763] Partition Labels
+#
+
+# @lc code=start
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        d = {}
+        for i, ch in enumerate(s):
+            d[ch] = i
+        mx = 0
+        res = []
+        prev = 0
+        for i, ch in enumerate(s):
+            mx = max(mx, d[ch])
+            if mx == i:
+                res.append(d[ch] + 1 - prev)
+                prev += res[-1]
+                mx = 0
+        return res
+# @lc code=end
+
