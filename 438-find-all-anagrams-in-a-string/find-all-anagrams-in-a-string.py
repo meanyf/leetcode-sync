4b64756d8b748ce2f10f24db67b39cb6e2d67bbd
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-28 13:25:30 +0300

    Time: 43 ms (67.16%) | Memory: 18.3 MB (79.04%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -14,32 +14,32 @@ class Solution:
         res = []
         for item in p:
             d[item] += 1
-        i = 0
         l = r = 0
         d2 = defaultdict(int)
+        target = 0
         while l < len(s) and r < len(s):
             d2[s[r]] += 1
+            target += 1
             if s[r] in d and d2[s[r]] <= d[s[r]]:
-                if len(d2) == len(d) and d2 == d:
+                if target == len(p):
                     res.append(l)
                     if d2[s[l]] > 0:
                         d2[s[l]] -= 1
                     l += 1
+                    target -= 1
             else:
                 if s[r] not in d:
                     l = r + 1
                     d2.clear()
+                    target = 0
                 else:
                     el = s[r]
                     while d2[el] != d[el]:
                         if d2[s[l]] > 0:
                             d2[s[l]] -= 1
+                            target -= 1
                         l += 1
             r += 1
         return res
-        
-
-            
-
 # @lc code=end
 
