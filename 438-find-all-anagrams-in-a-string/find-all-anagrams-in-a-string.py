de7f0be18c1b5c2b2c310b31e84e17eba1b397bf
meanyf <112635638+meanyf@users.noreply.github.com>
2025-06-28 10:57:54 +0300

    Time: 47 ms (61.16%) | Memory: 18.2 MB (79.04%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=438 lang=python3
+#
+# [438] Find All Anagrams in a String
+#
+
+# @lc code=start
+from collections import defaultdict
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if not s:
+            return []
+        d = defaultdict(int)
+        res = []
+        for item in p:
+            d[item] += 1
+        i = 0
+        l = r = 0
+        d2 = defaultdict(int)
+        while l < len(s) and r < len(s):
+            d2[s[r]] += 1
+            if s[r] in d and d2[s[r]] <= d[s[r]]:
+                if len(d2) == len(d) and d2 == d:
+                    res.append(l)
+                    if d2[s[l]] > 0:
+                        d2[s[l]] -= 1
+                    l += 1
+                    r += 1
+                else:
+                    r += 1
+            else:
+                if s[r] not in d:
+                    r += 1
+                    l = r
+                    d2.clear()
+                else:
+                    el = s[r]
+                    while d2[el] != d[el]:
+                        if d2[s[l]] > 0:
+                            d2[s[l]] -= 1
+                        l += 1
+                    r += 1
+        return res
+        
+
+            
+
+# @lc code=end
+
