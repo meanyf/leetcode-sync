f5eb1a1bfc03977456c63e5f76ed583881fecb4f
meanyf <112635638+meanyf@users.noreply.github.com>
2025-07-11 10:28:44 +0300

    Time: 3 ms (37.26%) | Memory: 17.9 MB (51.62%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=71 lang=python3
+#
+# [71] Simplify Path
+#
+
+# @lc code=start
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        lst = path.split('/')
+        res = ['/']
+        print(lst)
+        for item in lst:
+            if res and item == '..':
+                if ''.join(res) != '/':
+                    res.pop()
+            elif item == '.':
+                continue
+            elif item:
+                res.append(item + '/')
+        ans = ''.join(res)
+        print(ans)
+        if len(ans) > 1 and ans[-1] == '/':
+            return ans[:-1]
+        if not ans:
+            return '/'
+        
+        return ans
+# @lc code=end
