66962f3ea5c29a7ef4ea9c71966f1b7ee7c1749f
meanyf <112635638+meanyf@users.noreply.github.com>
2025-07-16 16:20:38 +0300

    Time: 293 ms (89.93%) | Memory: 17.8 MB (48.70%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -1,31 +1,29 @@
+# @lc app=leetcode id=16 lang=python3
+#
+# [16] 3Sum
+#
+
+# @lc code=start
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        closest_sum = float('inf')
-        min_diff = float('inf')
-        n = len(nums)
-        
-        for idx in range(n - 2):
-            if idx > 0 and nums[idx] == nums[idx - 1]:
+        res = float('inf')
+        mn = float('inf')
+        for idx in range(len(nums) - 2):
+            if idx > 0 and nums[idx] == nums[idx-1]:
                 continue
-            l, r = idx + 1, n - 1
+            l = idx + 1
+            r = len(nums) - 1
             while l < r:
-                current_sum = nums[idx] + nums[l] + nums[r]
-                current_diff = abs(target - current_sum)
-                
-                if current_diff < min_diff:
-                    min_diff = current_diff
-                    closest_sum = current_sum
-                
-                if current_sum == target:
-                    return current_sum
-                elif current_sum < target:
+                val = nums[idx] + nums[l] + nums[r]
+                if abs(target - val) < mn:
+                    mn = abs(target - val)
+                    res = val
+                    if target == val: return target
+                elif val < target:
+                    prev = nums[l]
                     l += 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
                 else:
                     r -= 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
-                        
-        return closest_sum
+        return res
+# @lc code=end
