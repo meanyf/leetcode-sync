3db710575e97e1484cf530018598c5927454a763
meanyf <112635638+meanyf@users.noreply.github.com>
2025-07-16 10:05:28 +0300

    Time: 1075 ms (15.15%) | Memory: 21.7 MB (11.84%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -10,25 +10,26 @@ class Solution:
         nums.sort()
         idx = 0
         res = []
-        prev = []
-        print(nums)
         s = set()
         while idx < len(nums) - 2:
             l = idx + 1
             r = len(nums) - 1
             while l < r:
-                if nums[idx] + nums[l] + nums[r] == 0:
-                    if [nums[idx], nums[l], nums[r]] != prev and ''.join(map(str, [nums[idx], nums[l], nums[r]])) not in s:
-                        s.add(''.join(map(str, [nums[idx], nums[l], nums[r]])))
-                        res.append([nums[idx], nums[l], nums[r]])
-                    prev = [nums[idx], nums[l], nums[r]]
-                # if nums[l] == nums[r]:
-                #     break
-                if nums[idx] + nums[l] + nums[r] < 0:
+                val = nums[idx] + nums[l] + nums[r]
+                if val == 0:
+                    item = [nums[idx], nums[l], nums[r]]
+                    key = (nums[idx], nums[l], nums[r])
+                    if key not in s:
+                        s.add(key)
+                        res.append(item)
+                if nums[l] == nums[r]:
+                    break
+                if val < 0:
                     l += 1
                 else:
                     r -= 1
             idx += 1
         return res
 # @lc code=end
+
 
