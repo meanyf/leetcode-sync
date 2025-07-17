5490383f06b08fe9cd8851d97c436593e4c66ed0
meanyf <112635638+meanyf@users.noreply.github.com>
2025-07-17 20:58:20 +0300

    Time: 47 ms (9.97%) | Memory: 20.2 MB (92.45%) - LeetSync

---
+++ 2352-design-an-atm-machine/design-an-atm-machine.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=2241 lang=python3
+#
+# [2241] Design an ATM Machine
+#
+
+# @lc code=start
+from collections import defaultdict
+class ATM:
+
+    def __init__(self):
+        self.d = defaultdict(int)
+    def deposit(self, banknotesCount: List[int]) -> None:
+        self.d[20] += banknotesCount[0]
+        self.d[50] += banknotesCount[1]
+        self.d[100] += banknotesCount[2]
+        self.d[200] += banknotesCount[3]
+        self.d[500] += banknotesCount[4]
+
+    def withdraw(self, amount: int) -> List[int]:
+        self.back = defaultdict(int)
+
+        res = [0] * 5
+        for i in range(5):
+            current_banknote = None
+            if i == 0: current_banknote = 500
+            if i == 1: current_banknote = 200
+            if i == 2: current_banknote = 100
+            if i == 3: current_banknote = 50
+            if i == 4: current_banknote = 20
+            if current_banknote <= amount and self.d[current_banknote] > 0:
+                target = min(amount // current_banknote, self.d[current_banknote])
+                self.back[current_banknote] += target
+                res[i] += target
+                amount -= current_banknote * target
+                self.d[current_banknote] -= target
+        print(amount)
+        if amount == 0:
+            res.reverse()
+            return res
+        for key in self.back:
+            self.d[key] += self.back[key]
+            self.back[key] = 0 
+        return [-1]
+        
+      
+
+
+
+        ...
+
+
+# Your ATM object will be instantiated and called as such:
+# obj = ATM()
+# obj.deposit(banknotesCount)
+# param_2 = obj.withdraw(amount)
+# @lc code=end
+
