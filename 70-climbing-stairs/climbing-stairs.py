176782dff3acee829bbf507b0bb23b9131e0c36e
meanyf <112635638+meanyf@users.noreply.github.com>
2025-07-23 19:34:23 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (8.11%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -7,4 +7,12 @@ class Solution:
             if n < 0: return 0
             return run(n - 2) + run(n - 1)
         
-        return run(n)
+        # return run(n)
+
+        
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        if n > 0: dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
