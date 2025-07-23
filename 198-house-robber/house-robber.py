b845332f5cabd82806c19b54c4d4a44072f8883e
meanyf <112635638+meanyf@users.noreply.github.com>
2025-07-23 19:46:53 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.14%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -9,9 +9,9 @@ from functools import cache
 class Solution:
     def rob(self, nums: List[int]) -> int:
         @cache
-        def run(i, robbed, res):
+        def run(i):
             if i >= len(nums): return 0
-            return max(nums[i] + run(i + 2, 0, 0), run(i + 1, 0, 0))
-        return run(0, 0, 0)
+            return max(nums[i] + run(i + 2), run(i + 1))
+        return run(0)
 # @lc code=end
 
