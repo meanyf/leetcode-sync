5ae3d18d304173a147f8f4a58f81453b9263ffc2
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-06 11:21:41 +0300

    Time: 136 ms (29.11%) | Memory: 18.4 MB (15.84%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -14,29 +14,27 @@ class Solution:
             d[item] = d.get(item, 0) + 1
         ans = ""
         window = {}
-        win2 = {}
         res = float("inf")
         l = 0
+        formed = 0
         for r, ch in enumerate(s):
-            if l > r: break
+            if l > r:
+                break
             if ch in d:
                 window[ch] = window.get(ch, 0) + 1
+                if window[ch] == d[ch]:
+                    formed += 1
 
-            if len(window) == len(d):
-                x = True
-                while x and l < len(s):
-                    for item in d:
-                        if d[item] > window[item]:
-                            x = False
-                            break
-                    if x is False: break
-                    if r - l + 1 < res:
-                        res = r - l + 1
-                        ans = s[l : r + 1]
-                    if s[l] in window:
-                        window[s[l]] -= 1
-                    l += 1
-                    # print(s[l : r + 1])
+            while l < len(s) and formed == len(d):
+                if r - l + 1 < res:
+                    res = r - l + 1
+                    ans = s[l : r + 1]
+                if window.get(s[l], 0) == d.get(s[l], 0) and window.get(s[l], 0) != 0:
+                    formed -= 1
+                if s[l] in window:
+                    window[s[l]] -= 1
+                l += 1
+                # print(s[l : r + 1])
 
         return ans
 
