017e6575d9646ed8ffc550562ad2a5fe0613502e
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-06 10:06:38 +0300

    Time: 19 ms (65.54%) | Memory: 17.9 MB (39.61%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,16 +2,18 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if not s2 or len(s2) < len(s1):
             return False
-        d = defaultdict(int)
-        window = defaultdict(int)
+        d = {}
+        window = {}
         for item in s1:
-            d[item] += 1
+            d[item] = d.get(item, 0) + 1
         l = 0
         for r, ch in enumerate(s2):
-            window[ch] += 1
+            window[ch] = window.get(ch, 0) + 1
             
-            while window[ch] > d[ch]:
-                window[s2[l]] -= 1
+            
+            while window[ch] > d.get(ch, 0):
+                if s2[l] in window:
+                    window[s2[l]] -= 1
                 l += 1
                 
             if r - l + 1 == len(s1):
