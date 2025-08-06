729535fe550333ff229eb682f9296117fce1ce84
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-06 11:43:02 +0300

    Time: 80 ms (49.53%) | Memory: 18.3 MB (54.33%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -13,28 +13,25 @@ class Solution:
         for item in t:
             d[item] = d.get(item, 0) + 1
         window = {}
-        res = float("inf")
-        ans_l = ans_r = 0
+        ans = (float("inf"), 0, 0)
         l = 0
         formed = 0
         for r, ch in enumerate(s):
+            window[ch] = window.get(ch, 0) + 1
             if ch in d:
-                window[ch] = window.get(ch, 0) + 1
                 if window[ch] == d[ch]:
                     formed += 1
 
             while l < len(s) and formed == len(d):
-                if r - l + 1 < res:
-                    res = r - l + 1
-                    ans_l = l
-                    ans_r = r
+                if r - l + 1 < ans[0]:
+                    ans = (r - l + 1, l, r)
                 if window.get(s[l], 0) == d.get(s[l], 0) and window.get(s[l], 0) != 0:
                     formed -= 1
                 if s[l] in window:
                     window[s[l]] -= 1
                 l += 1
 
-        return "" if res == float("inf") else s[ans_l : ans_r + 1]
+        return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
 
 
 # @lc code=end
