8f1cf3f7cdf3110928d98e05935e622144effc9b
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-10 11:19:54 +0300

    Time: 6 ms (51.67%) | Memory: 18.8 MB (93.32%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -7,34 +7,67 @@
 
 # @lc code=start
 from collections import defaultdict
+
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         visited = set()
         visiting = set()
-        nodes = set()
+        nodes = range(numCourses)
         g = defaultdict(list)
         for item, val in prerequisites:
             g[val].append(item)
-            nodes.add(item)
-            nodes.add(val)
 
-        def dfs(node):
-            visited.add(node)
-            visiting.add(node)
-            for nei in g[node]:
-                if nei in visiting:
+        for start in nodes:
+            if start in visited:
+                continue
+            stack = [(start, False)]
+            while stack:
+                node, expanded = stack.pop()
+                if expanded:
+                    visited.add(node)
+                    visiting.remove(node)
+                    continue
+
+                if node in visiting:
                     return False
-                if nei not in visited:
-                    if not dfs(nei):
+                if node in visited:
+                    continue
+
+                visiting.add(node)
+                stack.append((node, True))
+                for nei in g[node]:
+                    if nei in visiting:
                         return False
-            visiting.discard(node)
-            return True
-        
-        for node in nodes:
-            if node not in visited:
-                if not dfs(node): 
-                    return False
+                    stack.append((nei, False))
+
         return True
+        # visited = set()
+        # visiting = set()
+        # nodes = range(numCourses)
+        # g = defaultdict(list)
+        # for item, val in prerequisites:
+        #     g[val].append(item)
+
+        # def dfs(node):
+        #     if node in visiting:
+        #         return False
+        #     if node in visited:
+        #         return True
+
+        #     visiting.add(node)
+        #     for nei in g[node]:
+        #         if not dfs(nei):
+        #             return False
+
+        #     visiting.discard(node)
+        #     visited.add(node)
+        #     return True
+
+        # for node in nodes:
+        #     if not dfs(node):
+        #         return False
+        # return True
 
 
 # @lc code=end
