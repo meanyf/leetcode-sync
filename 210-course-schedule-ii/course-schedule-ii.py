f1b2a1cc57a3bb9cc56cb899b789b411bc2941f3
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-10 12:06:16 +0300

    Time: 1 ms (84.94%) | Memory: 19.2 MB (49.15%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=210 lang=python3
+#
+# [210] Course Schedule II
+#
+
+# @lc code=start
+from collections import defaultdict
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        res = []
+        visited = set()
+        visiting = set()
+        nodes = range(numCourses)
+        g = defaultdict(list)
+        for item, val in prerequisites:
+            g[val].append(item)
+
+        for start in nodes:
+            if start in visited:
+                continue
+            stack = [(start, False)]
+            while stack:
+                node, expanded = stack.pop()
+
+                if expanded:
+                    visited.add(node)
+                    visiting.remove(node)
+                    res.append(node)
+                    continue
+
+                if node in visiting:
+                    return False
+                if node in visited:
+                    continue
+                    
+                visiting.add(node)
+                stack.append((node, True))
+                for nei in g[node]:
+                    if nei in visiting:
+                        return []
+                    if nei in visited:
+                        continue
+                    stack.append((nei, False))
+
+        return res[::-1]
+# @lc code=end
