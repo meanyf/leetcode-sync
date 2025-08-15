dae76142313ae4587991c57a14072c9391397e34
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-15 14:04:39 +0300

    Time: 41 ms (98.66%) | Memory: 20.5 MB (31.13%) - LeetSync

--- 341-flatten-nested-list-iterator/flatten-nested-list-iterator.py
+++ 341-flatten-nested-list-iterator/flatten-nested-list-iterator.py
@@ -2,7 +2,7 @@
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
-#class NestedInteger:
+# class NestedInteger:
 #    def isInteger(self) -> bool:
 #        """
 #        @return True if this NestedInteger holds a single integer, rather than a nested list.
@@ -20,32 +20,28 @@
 #        Return None if this NestedInteger holds a single integer
 #        """
 from collections import deque
+
 class NestedIterator:
-    def __init__(self, nestedList: [NestedInteger]):    
+    def __init__(self, nestedList: [NestedInteger]):
         self.stack = deque(nestedList)
         self.res = []
         self.i = 0
-        k = 0
         while self.stack:
             item = self.stack.popleft()
             if isinstance(item, int):
                 self.res.append(item)
                 continue
             if isinstance(item, list):
-                # print('list', k, item)
                 self.stack.extendleft(item[::-1])
                 continue
             if isinstance(item.getInteger(), int):
                 self.stack.appendleft(item.getInteger())
             if item.getList():
-                # print(k, item)
                 self.stack.appendleft(item.getList())
-            k += 1
-        print('fewfwewe')
-        print(self.res)
+
     def next(self) -> int:
         return self.res[self.i - 1]
-    
+
     def hasNext(self) -> bool:
         self.i += 1
         return self.i <= len(self.res)
