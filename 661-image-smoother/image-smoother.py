e0e8c76e3982d0f082802a816c7af9a4f8c8a063
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-16 09:49:38 +0300

    Time: 168 ms (47.14%) | Memory: 19 MB (14.06%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -2,15 +2,12 @@ import math
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
         dirs = [(0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1), (1, 0), (-1, 0), (0, 0)]
-        if not img:
-            return []
-        col = len(img[0])
-        # row = len(img)
-        res = [[None] * col for row in range(len(img))]
+
+        cols = len(img[0])
+        res = [[None] * cols for row in range(len(img))]
         for row in range(len(img)):
             for col in range(len(img[0])):
-                cnt = 0
-                ans = 0
+                ans = cnt = 0
                 for x, y in dirs:
                     if -1 < row + y < len(img) and -1 < col + x < len(img[0]):
                         ans += (img[row + y][col + x]) 
