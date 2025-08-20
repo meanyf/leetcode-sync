b6aeaf23ed421c84f50ef5fdf73d53890a72cee9
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-20 10:41:25 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.84%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -8,14 +8,45 @@
 # @lc code=start
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        cnt = 0
-        for i in range(len(haystack) - len(needle) + 1):
-            for j in range(len(needle)):
-                if haystack[i + cnt] == needle[j]:
-                    cnt += 1
-            if cnt == len(needle):
-                return i 
-            cnt = 0
+        lps = [0] * len(needle)
+        i = 1
+        length = 0
+        while i < len(needle):
+            if needle[i] == needle[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    i += 1
+        print(lps)
+
+        i = j = 0
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+            else:
+                if j > 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+            if j == len(needle):
+                break
+        else:
+            return -1
+        return i - len(needle)
+
+        # cnt = 0
+        # for i in range(len(haystack) - len(needle) + 1):
+        #     for j in range(len(needle)):
+        #         if haystack[i + cnt] == needle[j]:
+        #             cnt += 1
+        #     if cnt == len(needle):
+        #         return i 
+        #     cnt = 0
         return -1
 
 
