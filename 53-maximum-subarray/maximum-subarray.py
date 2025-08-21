e1fac4edb35e6bca2b3fe5870ef468c736c54651
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-21 11:45:37 +0300

    Time: 303 ms (5.10%) | Memory: 175 MB (6.10%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -12,14 +12,12 @@ class Solution:
         n = len(nums)
         bst = float('-inf')
         @cache
-        def run(i, cur):
-            nonlocal bst
-            bst = max(bst, cur)
-            if i == n: 
-                return cur
-            if cur + nums[i] >= nums[i]:
-                return run(i + 1, cur + nums[i])
-            return run(i + 1, nums[i])
-        run(0, float('-inf'))
+        def run(i):
+            if i == n - 1: 
+                return nums[-1]
+            return max(nums[i], nums[i] + run(i + 1))
+            
+        for i in range(n):
+            bst = max(bst, run(i))
         return bst
 # @lc code=end
