cdac8f668dff82159785b502fa5fc86ee1d1c665
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-22 10:01:42 +0300

    Time: 43 ms (16.11%) | Memory: 28.5 MB (6.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -3,13 +3,12 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        bst = float('inf')
         @cache
         def run(x, y):
             if x == m - 1 and y == n - 1:
                 return grid[x][y]
-            if x < m and y < n:
-                return min(run(x + 1, y) + grid[x][y], grid[x][y] + run(x, y + 1))
-            return float('inf')
+            down = run(x + 1, y) + grid[x][y] if x + 1 < m else float('inf')
+            up = grid[x][y] + run(x, y + 1) if y + 1 < n else float('inf')
+            return min(down, up)
 
         return run(0, 0)
