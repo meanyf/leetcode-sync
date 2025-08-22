d22d1f9e1840e51f754f474edc6ad67c26c88bef
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-22 12:07:40 +0300

    Time: 0 ms (100.00%) | Memory: 18.4 MB (10.56%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -6,9 +6,12 @@ class Solution:
         n = len(grid[0])
         @cache
         def run(x, y):
-            if x == m - 1 and y == n - 1 and grid[x][y] == 0:
+            if x >= m or y >= n:
+                return 0
+            if grid[x][y] == 1:
+                return 0
+            if x == m - 1 and y == n - 1:
                 return 1
-            down = run(x + 1, y) if x + 1 < m and grid[x][y] == 0 and grid[x + 1][y] == 0 else 0
-            up = run(x, y + 1) if y + 1 < n and  grid[x][y] == 0 and grid[x][y + 1] == 0 else 0
-            return down + up
+
+            return run(x + 1, y) + run(x, y + 1)
         return run(0, 0)
