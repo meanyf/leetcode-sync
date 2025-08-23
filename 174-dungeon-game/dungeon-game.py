f1480ba7966878de14a56d90692dc4c60d0afdc1
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-23 12:29:09 +0300

    Time: 11 ms (28.69%) | Memory: 21.7 MB (5.40%) - LeetSync

--- 174-dungeon-game/dungeon-game.py
+++ 174-dungeon-game/dungeon-game.py
@@ -4,27 +4,22 @@ class Solution:
         grid = dungeon
         m = len(grid)
         n = len(grid[0])
-        for row in grid:
-            print(row)
         @cache
         def run(x, y):
             if x >= m or y >= n:
-                return float('inf'), -float('inf')
+                return -float('inf')
             if x == m - 1 and y == n - 1:
-                return grid[x][y], grid[x][y]
+                return grid[x][y]
             item = grid[x][y]
-            down, down_mn = run(x + 1, y)
-            right, right_mn = run(x, y + 1)
-            down += item
-            right += item
+            down_mn = run(x + 1, y)
+            right_mn = run(x, y + 1)
             if down_mn > right_mn:
-                return down, min(item, item + down_mn)
+                return min(item, item + down_mn)
             else:
-                return right, min(item, item + right_mn)
+                return min(item, item + right_mn)
 
 
-        dd, bst = run(0, 0)
-        print(bst)
+        bst = run(0, 0)
         if bst > 0:
             return 1
         return 1 - bst
