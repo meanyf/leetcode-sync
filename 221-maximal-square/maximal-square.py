1074f1c45b149c03e85f103fbcde1bb49de74f67
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-24 10:45:18 +0300

    Time: 397 ms (15.02%) | Memory: 120.9 MB (7.00%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -4,17 +4,12 @@ class Solution:
         grid = matrix
         m = len(grid)
         n = len(grid[0])
-        bst = -float('inf')
-        for row in grid:
-            print(row)
+        bst = 0
         @cache
         def run(x, y):
             nonlocal bst
             if x >= m or y >= n:
                 return 0
-            if x == m - 1 and y == n - 1:
-                bst = int(grid[x][y])
-                return int(grid[x][y])
             item = int(grid[x][y])
             side = min(run(x + 1, y), run(x, y + 1), run(x + 1, y + 1)) + item
             if item == 0:
