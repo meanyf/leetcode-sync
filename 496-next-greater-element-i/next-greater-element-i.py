7d09d45f3bfcc14bf89ed2d680d6ae87ed21d34c
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-24 16:19:45 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (40.54%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,12 +1,28 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res = [-1] * len(nums1)
-        for i, num in enumerate(nums1):
-            x = False
-            for item in nums2:
-                if item == num:
-                    x = True
-                elif x and item > num:
-                    res[i] = item
-                    break
+        if not nums2:
+            return res
+        stack = []
+        d = {}
+        nums2.reverse()
+        for item in nums2:
+            while stack and stack[-1] < item:
+                stack.pop()
+            if stack:
+                d[item] = stack[-1]
+            stack.append(item)
+        for i, item in enumerate(nums1):
+            if item in d:
+                res[i] = d[item]
         return res
+        # res = [-1] * len(nums1)
+        # for i, num in enumerate(nums1):
+        #     x = False
+        #     for item in nums2:
+        #         if item == num:
+        #             x = True
+        #         elif x and item > num:
+        #             res[i] = item
+        #             break
+        # return res
