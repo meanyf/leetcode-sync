d88df0387369c87d3fc1fc423b36a6aafd00a9e0
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-25 10:17:44 +0300

    Time: 40 ms (16.16%) | Memory: 19.3 MB (75.10%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -4,9 +4,9 @@ class Solution:
         stack = []
         for i in range(2*len(nums) - 1, -1, -1):
             item = nums[i % len(nums)]
-            while stack and stack[-1] <= item:
+            while stack and nums[stack[-1]] <= item:
                 stack.pop()
             if stack and i < len(nums):
-                res[i] = stack[-1]
-            stack.append(item)
+                res[i] = nums[stack[-1]]
+            stack.append(i % len(nums))
         return res
