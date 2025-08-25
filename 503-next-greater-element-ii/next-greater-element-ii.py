dab9568fd9562dc8bdccbc4f9769edce8997bcbc
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-25 10:11:18 +0300

    Time: 31 ms (31.45%) | Memory: 19.3 MB (75.10%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,11 +1,7 @@
-from collections import defaultdict
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         res = [-1] * len(nums)
-        if not nums:
-            return res
         stack = []
-        d = defaultdict(list)
         for i in range(2*len(nums) - 1, -1, -1):
             item = nums[i % len(nums)]
             while stack and stack[-1] <= item:
@@ -13,7 +9,4 @@ class Solution:
             if stack and i < len(nums):
                 res[i] = stack[-1]
             stack.append(item)
-        # for i, item in enumerate(nums):
-        #     if d[item]:
-        #         res[i] = d[item].pop()
         return res
