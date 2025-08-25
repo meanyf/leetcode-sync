6675b287ee972829f6f178515e642238798f34d8
meanyf <112635638+meanyf@users.noreply.github.com>
2025-08-25 09:58:30 +0300

    Time: 51 ms (12.65%) | Memory: 20.5 MB (6.22%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,32 +1,19 @@
 from collections import defaultdict
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        nums1 = nums[:]
-        nums2 = nums[:] + nums[:]
-        
-        res = [-1] * len(nums1)
-        if not nums2:
+        res = [-1] * len(nums)
+        if not nums:
             return res
         stack = []
         d = defaultdict(list)
-        for item in reversed(nums2):
+        for i in range(2*len(nums) - 1, -1, -1):
+            item = nums[i % len(nums)]
             while stack and stack[-1] <= item:
                 stack.pop()
             if stack:
                 d[item].append(stack[-1])
             stack.append(item)
-        print(d)
-        for i, item in enumerate(nums1):
+        for i, item in enumerate(nums):
             if d[item]:
                 res[i] = d[item].pop()
-        d = {}
-        # for item in nums2:
-        #     while stack and stack[-1] <= item:
-        #         stack.pop()
-        #     if stack:
-        #         d[item] = stack[-1]
-        #     stack.append(item)
-        # for i, item in enumerate(nums1):
-        #     if item in d and res[i] == -1:
-        #         res[i] = d[item]
         return res
