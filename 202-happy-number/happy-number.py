042fb2bca94644e82ec76ab6fa678db0c340f7ac
meanyf <112635638+meanyf@users.noreply.github.com>
2025-09-01 13:52:34 +0300

    Time: 3 ms (39.69%) | Memory: 17.6 MB (86.97%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,16 +1,14 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        s = str(n)
-        res = list(s)
+        res = str(n)
         used = set()
         while True:
             ans = 0
-            for item in res:
-                ans += int(item)**2
+            ans = sum(int(item)**2 for item in res)
             if ans in used:
                 return False
             used.add(ans)
             if ans == 1:
                 return True
-            res = list(str(ans))
+            res = str(ans)
         return False
