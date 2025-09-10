7967f24eefe60f53fe65f0875e622068bacd93d7
meanyf <112635638+meanyf@users.noreply.github.com>
2025-09-10 09:06:15 +0300

    Time: 0 ms (100.00%) | Memory: 18.3 MB (79.55%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,23 +3,35 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        res = []
-        zero_rows = set()
-        zero_cols = set()
+        first_row_zero = first_col_zero = False
+        for j in range(len(matrix[0])):
+            if matrix[0][j] == 0:
+                first_row_zero = True
+                break
+        
         for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
+            if matrix[i][0] == 0:
+                first_col_zero = True
+                break
+        
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[i])):
                 if matrix[i][j] == 0:
-                    zero_rows.add(i)
-                    zero_cols.add(j)
-        for i in range(len(matrix)):
-            if i in zero_rows:
-                for j in range(len(matrix[i])):
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[i])):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
         
-        for j in range(len(matrix[0])):
-            if j in zero_cols:
-                for i in range(len(matrix)):
-                    matrix[i][j] = 0
+        if first_row_zero:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+
+        if first_col_zero:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0   
         
 
 
