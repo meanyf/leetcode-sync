a211ee7d6957ea82aeeab126aa27f4e183a61d3a
meanyf <112635638+meanyf@users.noreply.github.com>
2025-09-12 06:39:32 +0300

    Time: 274 ms (26.36%) | Memory: 25 MB (33.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -6,19 +6,20 @@ class Solution:
         visited = set()
         rows, cols = len(grid), len(grid[0])
         ans = 0
-        def dfs(r, c):
-            if r > rows - 1 or c > cols - 1: return 0
-            if r < 0 or c < 0: return 0
-            if grid[r][c] == '0' or (r, c) in visited: return 0
-            visited.add((r, c))
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == '1' and (i, j) not in visited:
-                    dfs(i, j)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r, c) not in visited:
+                    stack = [(r, c)]
+                    while stack:
+                        i, j = stack.pop()
+                        if i < 0 or i >= rows or j < 0 or j >= cols:
+                            continue
+                        if grid[i][j] == '0' or (i, j) in visited:
+                            continue
+                        visited.add((i, j))
+                        stack.append((i + 1, j))
+                        stack.append((i - 1, j))
+                        stack.append((i, j + 1))
+                        stack.append((i, j - 1))
                     ans += 1
         return ans
