315d0d43aa10afd0e819bb73661955f298de2ac9
meanyf <112635638+meanyf@users.noreply.github.com>
2025-09-17 09:09:57 +0300

    Time: 245 ms (60.04%) | Memory: 20.1 MB (82.69%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,32 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
-        visited = set()
+        if not grid and not grid[0]:
+            return []
         rows, cols = len(grid), len(grid[0])
-        ans = 0
+        visited = [[False] * cols for _ in range(rows)]
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        res = 0
+        def bfs(start_r: int, start_c: int) -> None:
+            q = deque()
+            q.append((r, c))
+            visited[start_r][start_c] = True
+            while q:
+                cur_r, cur_c = q.popleft()
+                for dr, dc in dirs:
+                    nr, nc = cur_r + dr, cur_c + dc
+                    if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
+                        continue
+                    if visited[nr][nc] or grid[nr][nc] == "0":
+                        continue
+                    visited[nr][nc] = True
+                    q.append((nr, nc))
+
+        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in visited:
-                    stack = [(r, c)]
-                    while stack:
-                        i, j = stack.pop()
-                        if i < 0 or i >= rows or j < 0 or j >= cols:
-                            continue
-                        if grid[i][j] == '0' or (i, j) in visited:
-                            continue
-                        visited.add((i, j))
-                        stack.append((i + 1, j))
-                        stack.append((i - 1, j))
-                        stack.append((i, j + 1))
-                        stack.append((i, j - 1))
-                    ans += 1
-        return ans
+                if grid[r][c] == '1' and not visited[r][c]:
+                    bfs(r, c)
+                    res += 1
+        return res
+
