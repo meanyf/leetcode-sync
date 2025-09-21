4ac3e39d08e88047e06da5b99a0eb17b160c9ad0
meanyf <112635638+meanyf@users.noreply.github.com>
2025-09-21 07:21:21 +0300

    Time: 126 ms (87.94%) | Memory: 18.2 MB (80.53%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -3,25 +3,24 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0]: return -1
         rows, cols = len(grid), len(grid[0])
-        res = 0
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
         visited = [[False] * cols for _ in range(rows)]
         def bfs(start_r, start_c):
             if grid[start_r][start_c] == 1: return -1
-            if grid[start_r][start_c] == 0 and rows == 1 and cols == 1: return 1
             dist = 1
             q = deque()
             q.append((start_r, start_c, dist))
+            visited[start_r][start_c] = True
             while q:
                 r, c, dist = q.popleft()
+                if r == rows - 1 and c == cols - 1:
+                    return dist
                 for dr, dc in dirs:
                     nr, nc = dr + r, dc + c
                     if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
                         continue
                     if visited[nr][nc] or grid[nr][nc] == 1:
                         continue
-                    if nr == rows - 1 and nc == cols - 1:
-                        return dist + 1
                     visited[nr][nc] = True
                     q.append((nr, nc, dist + 1))
             return -1
