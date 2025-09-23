b8b37cd729b1798cf814c531c45acd476c84d3aa
meanyf <112635638+meanyf@users.noreply.github.com>
2025-09-23 07:55:49 +0300

    Time: 115 ms (80.18%) | Memory: 20.4 MB (63.88%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,28 +1,22 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        grid = mat
-        if not grid or not grid[0]: return 0
+        if not mat or not mat[0]: return []
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        rows, cols = len(grid), len(grid[0])
-        visited = [[False] * cols for _ in range(rows)]
+        rows, cols = len(mat), len(mat[0])
+        res = [[-1] * cols for _ in range(rows)]
         q = deque()
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 0:
+                if mat[r][c] == 0:
                     q.append((r, c))
-                    visited[r][c] = True
+                    res[r][c] = 0
         while q:
             for _ in range(len(q)):
                 r, c = q.popleft()
                 for dr, dc in dirs:
                     nr, nc = r + dr, c + dc
-                    if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
-                        continue
-                    if grid[nr][nc] == 0 or visited[nr][nc]:
-                        continue
-                    if grid[r][c] != 0:
-                        grid[nr][nc] = grid[r][c] + 1
-                    visited[nr][nc] = True
-                    q.append((nr, nc))
-        return grid
+                    if 0 <= nr < rows and 0 <= nc < cols and res[nr][nc] == -1:
+                        res[nr][nc] = res[r][c] + 1
+                        q.append((nr, nc))
+        return res
                     
