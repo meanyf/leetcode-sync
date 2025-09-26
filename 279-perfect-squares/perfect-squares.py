25f8e8af8f38e7935de5b16ba56944583c88671d
meanyf <112635638+meanyf@users.noreply.github.com>
2025-09-26 08:04:10 +0300

    Time: 3230 ms (19.88%) | Memory: 33.7 MB (9.51%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -12,7 +12,7 @@ class Solution:
         @cache
         def run(n):
             if n == 0: return 0 # дошли до конца
-            res = float('inf')
+            res = n + 1
             for i in range(1, int(sqrt(n)) + 1):
                 res = min(res, 1 + run(n - i*i))
             return res
