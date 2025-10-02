a377ea4021b256e44929ce5396dbfe0a17721d67
meanyf <112635638+meanyf@users.noreply.github.com>
2025-10-02 08:33:32 +0300

    Time: 0 ms (100.00%) | Memory: 18.9 MB (6.01%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: list[int], target: int) -> int:
         lo = 0
         hi = len(nums) - 1
-        while lo <= hi:
+        while lo < hi: 
             mi = (hi + lo) // 2
             if nums[mi] < target:
                 lo = mi + 1
@@ -10,4 +10,4 @@ class Solution:
                 hi = mi - 1
             else:
                 return mi
-        return -1
+        return -1 if nums[lo] != target else lo
