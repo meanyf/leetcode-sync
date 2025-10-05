a22b0e64be31451995f15011d6c8bafef5f72567
meanyf <112635638+meanyf@users.noreply.github.com>
2025-10-05 06:53:39 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.29%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -1,8 +1,6 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         res = start
-        i = 1
-        for item in range(1, n):
+        for i in range(1, n):
             res = res ^ (start + 2 * i)
-            i += 1
         return res
