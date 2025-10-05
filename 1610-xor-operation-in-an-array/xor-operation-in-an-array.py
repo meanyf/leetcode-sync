b7f289f87e683ffd28e9f662639b5f4acf61cb64
meanyf <112635638+meanyf@users.noreply.github.com>
2025-10-05 06:53:14 +0300

    Time: 3 ms (8.72%) | Memory: 17.6 MB (80.29%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -3,7 +3,6 @@ class Solution:
         res = start
         i = 1
         for item in range(1, n):
-            print(start + 2 * i)
             res = res ^ (start + 2 * i)
             i += 1
         return res
