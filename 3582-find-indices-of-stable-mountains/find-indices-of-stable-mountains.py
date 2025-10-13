9b9c51304ef95a1b592543eaf4dc318288abe17a
meanyf <112635638+meanyf@users.noreply.github.com>
2025-10-13 06:58:26 +0300

    Time: 3 ms (17.59%) | Memory: 17.8 MB (57.23%) - LeetSync

---
+++ 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.py
@@ -0,0 +1,3 @@
+class Solution:
+    def stableMountains(self, height: List[int], threshold: int) -> List[int]:
+        return [i for i, item in enumerate(height) if i > 0 and height[i - 1] > threshold]
