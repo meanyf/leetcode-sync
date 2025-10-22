f0ccfa5aac4e8665667eca54317a7186760e0312
meanyf <112635638+meanyf@users.noreply.github.com>
2025-10-22 07:26:50 +0300

    Time: 55 ms (9.78%) | Memory: 21.2 MB (85.27%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,25 +1,21 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        n = len(matrix)
-
-        def count_and_last_le(x):
+        def feasible(x):
             cnt = 0
             last = -float('inf')
-            j = n - 1
-            for i in range(n):
-                while j >= 0 and matrix[i][j] > x:
-                    j -= 1
-                if j >= 0:
-                    cnt += (j + 1)
-                    last = max(last, matrix[i][j])
-            return cnt, last
-
+            for row in matrix:
+                for item in row:
+                    if x >= item:
+                        cnt += 1
+                    if cnt >= k:
+                        return cnt
+            return cnt
         lo, hi = matrix[0][0], matrix[-1][-1]
-
+        res = None
         while lo < hi:
             mi = (lo + hi) // 2
-            cnt, last = count_and_last_le(mi)
-            if cnt >= k:
+            res = feasible(mi)
+            if res >= k:
                 hi = mi
             else:
                 lo = mi + 1
