736b16137ba4fa30bf4f207d4731aec15a16ecb2
meanyf <112635638+meanyf@users.noreply.github.com>
2025-10-29 07:37:20 +0300

    Time: 4 ms (18.98%) | Memory: 18.1 MB (17.13%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.py
+++ 4058-compute-alternating-sum/compute-alternating-sum.py
@@ -1,6 +1,3 @@
 class Solution:
     def alternatingSum(self, nums: List[int]) -> int:
-        res = 0
-        for i, item in enumerate(nums):
-            res += item if i % 2 == 0 else - item
-        return res
+        return sum(item if i % 2 == 0 else - item for i, item in enumerate(nums))
