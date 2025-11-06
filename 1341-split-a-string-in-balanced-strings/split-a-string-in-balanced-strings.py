3dca6da12b01f7caec05724721c2da1d3802524c
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-06 07:59:53 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.57%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -2,10 +2,7 @@ class Solution:
     def balancedStringSplit(self, s: str) -> int:
         res = R = L = 0
         for item in s:
-            if item == 'R':
-                R += 1
-            else:
-                L += 1
-            if R == L:
-                res += 1
+            R += 1 if item == 'R' else 0
+            L += 1 if item == 'L' else 0
+            res += 1 if R == L else 0
         return res
