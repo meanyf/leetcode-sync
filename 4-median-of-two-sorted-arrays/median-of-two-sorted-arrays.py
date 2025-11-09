fe93860ab788e6f769bcc0a9f6f4801a136424fe
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-09 08:30:51 +0300

    Time: 28 ms (5.57%) | Memory: 18 MB (58.68%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -8,24 +8,19 @@ class Solution:
                 cnt += 1 if item <= x else 0
             for item in nums2:
                 cnt += 1 if item <= x else 0
-            print('cnt', cnt)
             return cnt
         def bin_search(target):
             ans = None
             lo = min(nums1[0] if nums1 else float('inf'), nums2[0] if nums2 else float('inf'))
             hi = max(nums1[-1] if nums1 else -float('inf'), nums2[-1] if nums2 else -float('inf'))
             while lo <= hi:
-                print('lo', 'hi', lo, hi)
                 mi = (lo + hi) // 2
-                print('mi', mi)
                 if feasible(mi) >= target:
                     ans = mi
                     hi = mi - 1
                 else:
                     lo = mi + 1
-            print('ans', ans)
             return ans
-        
         target = n // 2
         if n % 2 == 0:
             return (bin_search(target) + bin_search(target + 1)) / 2
