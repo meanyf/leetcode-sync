a7c88503fc2222c26b018b1c48413dd77ec0a9cd
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-10 09:04:18 +0300

    Time: 50 ms (77.94%) | Memory: 21.2 MB (94.14%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,10 +1,3 @@
-#
-# @lc app=leetcode id=236 lang=python3
-#
-# [236] Lowest Common Ancestor of a Binary Tree
-#
-
-# @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -14,34 +7,26 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return []
-        stack = [(root, [])]
-        res = []
-        res2 = []
-        while stack:
-            node, lst = stack.pop()
-            if node:
-                lst.append(node.val)
-                if node.val == q.val:
-                    res = list(lst)
-                if node.val == p.val:
-                    res2 = list(lst)
-                stack.append((node.right, list(lst)))
-                stack.append((node.left, list(lst)))
-        mn = min(len(res), len(res2))
-        ans = 0
-        for i in range(mn):
-            if res[i] == res2[i]:
-                ans = res[i]
         stack = [root]
+        d = {}
         while stack:
             node = stack.pop()
             if node:
-                if node.val == ans:
-                    return node
-                stack.append(node.right)
-                stack.append(node.left)
-        return None
-# @lc code=end
-
+                if node.right:
+                    d[node.right] = node
+                    stack.append(node.right)
+                if node.left:
+                    d[node.left] = node
+                    stack.append(node.left)
+        cur = p
+        st = set([cur])
+        while cur in d:
+            st.add(d[cur])
+            cur = d[cur]
+        cur = q
+        while cur in d:
+            if cur in st:
+                return cur
+            cur = d[cur]
+        return cur
+        
