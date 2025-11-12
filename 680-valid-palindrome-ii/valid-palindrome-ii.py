c98cd998d001930d0c07607fd43edc127189ba80
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-12 08:02:53 +0300

    Time: 63 ms (55.66%) | Memory: 18 MB (35.61%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,87 +1,20 @@
-#
-# @lc app=leetcode id=680 lang=python3
-#
-# [680] Valid Palindrome II
-#
-
-# @lc code=start
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def is_palind(l, r):
+        def isPalindrome(l, r) -> bool:
             while l < r:
                 if s[l] != s[r]:
                     return False
                 l += 1
                 r -= 1
             return True
-
-        l, r = 0, len(s) - 1
+        l = 0
+        r = len(s) - 1
+        cnt = 0
         while l < r:
             if s[l] != s[r]:
-                return is_palind(l, r - 1) or is_palind(l + 1, r)
+                cnt += 1
+                if cnt == 2: return False
+                return isPalindrome(l + 1, r) or isPalindrome(l, r - 1)
             l += 1
             r -= 1
         return True
-        # if len(s) == 1:
-        #     return True
-        # def j(l, r):
-        #     x = l
-        #     cnt = 0
-        #     while l >= 0 and r < len(s):
-        #         if s[l] != s[r]:
-        #             cnt += 1
-        #             if x == len(s) // 2 - 1:
-        #                 r += 1
-        #             else:
-        #                 l -= 1
-        #         else:
-        #             r += 1
-        #             l -= 1
-        #     if r == len(s) and l == -1:
-        #         return True
-        #     if cnt == 0:
-        #         return True
-        # def g(l, r):
-        #     x = l
-        #     cnt = 0
-
-        #     while l >= 0 and r < len(s):
-        #         if s[l] != s[r]:
-        #             cnt += 1
-        #             if x == len(s) // 2 - 1:
-        #                 r += 1
-        #             else:
-        #                 l -= 1
-        #         else:
-        #             r += 1
-        #             l -= 1
-        #     if x == len(s) // 2:
-        #         if l == 0 and r == len(s):
-        #             return True
-        #     if x == len(s) // 2 - 1:
-        #         if l == -1 and r == len(s) - 1:
-        #             return True
-        #     if r == len(s) and l == -1:
-        #         return True
-            
-        #     if cnt == 0:
-        #         return True
-
-        # if len(s) % 2 == 0:
-        #     if j(len(s) // 2 - 1, len(s) // 2 - 1):
-        #         return True
-        #     if j(len(s) // 2, len(s) // 2):
-        #         return True
-        #     return False
-
-        # else:
-        #     if g(len(s) // 2 - 1, len(s) // 2):
-        #         return True
-        #     if g(len(s) // 2, len(s) // 2 + 1):
-        #         return True
-        #     return False
-
-            
-
-            
-# @lc code=end
