855eafcae14b9d68ef003d0424ab738e06130a0e
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-18 08:50:38 +0300

    Time: 31 ms (61.89%) | Memory: 20.4 MB (38.56%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,23 +1,11 @@
-#
-# @lc app=leetcode id=560 lang=python3
-#
-# [560] Subarray Sum Equals K
-#
-
-# @lc code=start
-from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        d = defaultdict(int)
-        d[0] = 1
-        prefix = 0
-        res = 0
-        for num in nums:
-            prefix += num
-            if prefix - k in d:
-                res += d[prefix - k]
-            d[prefix] += 1
-        return res
-
-# @lc code=end
-
+        d = {}
+        sm = 0
+        cnt = 0
+        for item in nums:
+            d[sm] = d.get(sm, 0) + 1
+            sm += item
+            if sm - k in d:
+                cnt += d[sm - k]
+        return cnt
