c830544aa4c4f8d0498907374ec1fa06d36bc7f7
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-21 09:49:24 +0300

    Time: 0 ms (100.00%) | Memory: 17.5 MB (98.43%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
@@ -1,8 +1,4 @@
 class Solution:
     def countDigits(self, num: int) -> int:
         lst = map(int, str(num))
-        cnt = 0
-        for item in lst:
-            if num % item == 0:
-                cnt += 1
-        return cnt
+        return sum(num % item == 0 for item in lst)
