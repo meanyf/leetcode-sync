b08a43ae6476064e416b7031ccddc91fb08c2190
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-25 10:18:15 +0300

    Time: 103 ms (58.12%) | Memory: 23.3 MB (88.33%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        d = {}
+        d = {0: -1}
         mx = sm = 0
         for i, item in enumerate(nums):
             sm += 1 if item == 0 else -1
@@ -8,6 +8,4 @@ class Solution:
                 mx = max(mx, i - d[sm])
             else:
                 d[sm] = i
-            if sm == 0:
-                mx = max(mx, i + 1)
         return mx
