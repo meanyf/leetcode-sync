a0cb38b81bfdea7b621ff714be91997a8d126a3a
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-25 09:29:52 +0300

    Time: 75 ms (14.40%) | Memory: 35.4 MB (95.74%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,35 +1,24 @@
-#
-# @lc app=leetcode id=523 lang=python3
-#
-# [523] Continuous Subarray Sum
-#
-
-# @lc code=start
-from collections import defaultdict
-
-
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        if len(nums) <= 1:
+        if len(nums) == 1:
             return False
-        d = defaultdict(int)
-        d[0] = -1
-        prefix = 0
-        cnt_zer = 0
-        for i, num in enumerate(nums):
-            prefix += num
-            if num == 0:
-                cnt_zer += 1
-            else:
-                cnt_zer = 0
-            if cnt_zer > 1:
-                return True
-            if prefix % k in d:
-                if i - d[prefix % k] >= 2:
+        d = {0: 1}
+        cnt = sm = 0
+        zero_cnt = 0
+        
+        for i, item in enumerate(nums):
+            sm += item
+            if sm % k in d:
+                if item % k == 0:
+                    if (sm - item) % k == 0 and i > 0:
+                        return True
+                    if zero_cnt > 0:
+                        return True
+                else:
                     return True
+            d[sm % k] = d.get(sm % k, 0) + 1
+            if item == 0:
+                zero_cnt += 1
             else:
-                d[prefix % k] = i
+                zero_cnt = 0
         return False
-
-
-# @lc code=end
