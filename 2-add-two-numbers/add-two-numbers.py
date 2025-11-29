4d655e58e91dee6bb87704b1142492f1e3603284
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-29 10:01:40 +0300

    Time: 4 ms (54.94%) | Memory: 17.9 MB (35.41%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,10 +9,8 @@ class Solution:
         carry = 0
         while l1 or l2 or carry:
             ans = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
-            carry = 0
-            if ans > 9:
-                ans = ans % 10
-                carry = 1
+            carry = ans // 10
+            ans = ans % 10
             cur.next = ListNode(ans)
             cur = cur.next
             l1 = l1.next if l1 else None
