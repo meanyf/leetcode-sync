d85d9d4b092484b358203a02763178e3d2004650
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-29 09:46:55 +0300

    Time: 3 ms (73.10%) | Memory: 17.8 MB (84.57%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,30 +5,19 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def reverse_list(l):
-            prev = None
-            while l:
-                nxt = l.next
-                l.next = prev
-                prev = l
-                l = nxt
-            return prev
-
         head = cur = ListNode()
         add = 0
         prev = None
-        while l1 or l2:
+        while l1 or l2 or add:
             ans = (l1.val if l1 else 0) + (l2.val if l2 else 0) + add
             add = 0
             if ans > 9:
                 ans = ans % 10
                 add = 1
-            cur.val = ans
-            prev = cur
-            cur.next = ListNode(1)
+            cur.next = ListNode(ans)
             cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        if not add:
-            prev.next = None
-        return head
+        if add:
+            cur.next = ListNode(1)
+        return head.next
