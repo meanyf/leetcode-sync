52adb1587a3c80372e93c563f1704065ac04a864
meanyf <112635638+meanyf@users.noreply.github.com>
2025-11-30 12:59:35 +0300

    Time: 49 ms (54.52%) | Memory: 19.3 MB (16.03%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -7,69 +7,53 @@ class Node:
 class MyLinkedList:
 
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.dummy = Node()
         self.size = 0
 
     def get(self, index: int) -> int:
         if index >= self.size: return -1
-        cur = self.head
+        cur = self.dummy.next
         for _ in range(index):
             cur = cur.next
         return cur.val
 
     def addAtHead(self, val: int) -> None:
-        self.head = Node(val, next=self.head)
-        if self.size == 0:
-            self.tail = self.head
-        else:
-            self.head.next.prev = self.head
+        self.dummy.next = Node(val, next=self.dummy.next, prev=self.dummy)
+        if self.size > 0: 
+            self.dummy.next.next.prev = self.dummy.next
         self.size += 1
         
     def addAtTail(self, val: int) -> None:
-        if self.size == 0:
-            self.head = self.tail = Node(val)
-        else:
-            self.tail.next = Node(val, prev=self.tail)
-            self.tail = self.tail.next
+        cur = self.dummy
+        while cur.next:
+            cur = cur.next
+        cur.next = Node(val, prev=cur)
         self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index > self.size: return None
-        if self.size == 0:
-            self.head = self.tail = Node(val)
-        elif index == 0:
-            self.head = Node(val, self.head)
-            self.head.next.prev = self.head
-        elif self.size == index:
-            self.tail.next = Node(val, prev=self.tail)
-            self.tail = self.tail.next
-        else:
-            cur = self.head
-            for _ in range(index - 1):
-                cur = cur.next
-            cur.next = Node(val, next=cur.next, prev=cur)
+        if index > self.size: 
+            return 
+        cur = self.dummy
+        for _ in range(index):
+            cur = cur.next
+        cur.next = Node(val, next=cur.next, prev=cur)
+        if cur.next.next is not None:
             cur.next.next.prev = cur.next
         self.size += 1
         
 
 
     def deleteAtIndex(self, index: int) -> None:
-        if index >= self.size: return None
-        if self.size == 1: 
-            self.head = self.tail = None
-        elif index == 0:
-            self.head = self.head.next
-            self.head.prev = None
-        elif index == self.size - 1:
-            self.tail = self.tail.prev
-            self.tail.next = None
-        else:
-            cur = self.head
-            for _ in range(index - 1):
-                cur = cur.next
-            cur.next = cur.next.next
-            cur.next.prev = cur
+        if index >= self.size: 
+            return 
+        if self.size == 0:
+            return
+        cur = self.dummy.next
+        for _ in range(index):
+            cur = cur.next
+        cur.prev.next = cur.next
+        if self.size > 1 and index != self.size - 1:
+            cur.next.prev = cur.prev
         self.size -= 1
         
 
