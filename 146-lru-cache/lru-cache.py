7db98fa88e597fd8203d8447f452eabc54287611
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-02 10:49:00 +0300

    Time: 121 ms (74.55%) | Memory: 78 MB (71.27%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,32 +1,64 @@
-#
-# @lc app=leetcode id=146 lang=python3
-#
-# [146] LRU Cache
-#
+class Node:
+    def __init__(self, val=0, key=0, next=None, prev=None):
+        self.val = val
+        self.key = key
+        self.next = next
+        self.prev = prev
 
-# @lc code=start
-from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.od = OrderedDict()
+        self.left = Node()
+        self.right = Node()
+        self.left.next = self.right
+        self.right.prev = self.left
+        self.d = {}
         self.capacity = capacity
 
     def get(self, key: int) -> int:
-        if key in self.od:
-            self.od.move_to_end(key)
-            return self.od[key]
-        return -1
+        if len(self.d) == 0:
+            return -1
+        if key not in self.d:
+            return -1
+        node = self.d[key]
+        nxt, prev = node.next, node.prev
+        prev.next = nxt
+        nxt.prev = prev
+
+        nxt, prev = self.left.next, self.left
+        prev.next = node
+        nxt.prev = node
+        node.prev = prev
+        node.next = nxt
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        self.od[key] = value
-        self.od.move_to_end(key)
-        if len(self.od) > self.capacity:
-            self.od.popitem(last=False)
+        if key in self.d:
+            self.d[key].val = value
+            self.get(key)
+            return
+        if len(self.d) == self.capacity:
+            del_node = self.right.prev
+            nxt, prev = del_node.next, del_node.prev
+            prev.next = nxt
+            nxt.prev = prev
+            self.d.pop(del_node.key, None)
+
+            node, nxt, prev = Node(val=value, key=key), self.left.next, self.left
+            nxt.prev = node
+            prev.next = node
+            node.prev = prev
+            node.next = nxt
+            self.d[key] = node
+        else:
+            node, nxt, prev = Node(val=value, key=key), self.left.next, self.left
+            nxt.prev = node
+            prev.next = node
+            node.prev = prev
+            node.next = nxt
+            self.d[key] = node
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
-# @lc code=end
-
