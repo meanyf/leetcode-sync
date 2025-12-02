4af06607fb720aea226aa281bdfdf9d8d9064d13
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-02 19:55:16 +0300

    Time: 0 ms (100.00%) | Memory: 18.9 MB (93.38%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,48 +1,24 @@
-#
-# @lc app=leetcode id=34 lang=python3
-#
-# [34] Find First and Last Position of Element in Sorted Array
-#
-
-# @lc code=start
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        res = [0, 0]
-        def lower_bound(nums, target):
-            lo, hi = 0, len(nums) - 1
-            while lo <= hi:
-                mi = (lo + hi) // 2
-                if nums[mi] < target:
-                    lo = mi + 1
-                else:
-                    hi = mi - 1
-            return lo
-        def upper_bound(nums, target):
-            lo, hi = 0, len(nums) - 1
-            while lo <= hi:
-                mi = (lo + hi) // 2
-                if nums[mi] <= target:
-                    lo = mi + 1
-                else:
-                    hi = mi - 1
-            return lo
-        def exact_match(nums, target):
-            lo, hi = 0, len(nums) - 1
-            while lo <= hi:
-                mi = (lo + hi) // 2
-                if nums[mi] == target:
-                    return True
-                elif nums[mi] < target:
-                    lo = mi + 1
-                else:
-                    hi = mi - 1
-            return False
-        if exact_match(nums, target):
-            res[0] = lower_bound(nums, target)
-            res[1] = upper_bound(nums, target) - 1
-            return res
-        else:
-            return [-1, -1]
-        
-        
-# @lc code=end
+        lo, hi = 0, len(nums) - 1
+        left = right = None
+        while lo <= hi:
+            mi = (lo + hi) // 2
+            if target == nums[mi]:
+                lo = mi + 1
+                right = mi
+            if target < nums[mi]:
+                hi = mi - 1
+            elif target > nums[mi]:
+                lo = mi + 1
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mi = (lo + hi) // 2
+            if target == nums[mi]:
+                hi = mi - 1
+                left = mi
+            if target < nums[mi]:
+                hi = mi - 1
+            elif target > nums[mi]:
+                lo = mi + 1
+        return [left if left is not None else -1, right if right is not None else -1]
