93e1c1580e8c2d0079c4b86566e7851c1e1c0e56
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-02 20:08:17 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (20.16%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,19 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        lo, hi = 0, len(nums) - 1
-        left = right = None
-        while lo <= hi:
-            mi = (lo + hi) // 2
-            if target == nums[mi]:
-                lo = mi + 1
-                right = mi
-            if target < nums[mi]:
-                hi = mi - 1
-            elif target > nums[mi]:
-                lo = mi + 1
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
-            mi = (lo + hi) // 2
-            if target == nums[mi]:
-                hi = mi - 1
-                left = mi
-            if target < nums[mi]:
-                hi = mi - 1
-            elif target > nums[mi]:
-                lo = mi + 1
-        return [left if left is not None else -1, right if right is not None else -1]
+        def bin_s(nums, left_bias):
+            lo, hi = 0, len(nums) - 1
+            res = -1
+            while lo <= hi:
+                mi = (lo + hi) // 2
+                if target < nums[mi]:
+                    hi = mi - 1
+                elif target > nums[mi]:
+                    lo = mi + 1
+                else:
+                    res = mi
+                    if left_bias:
+                        hi = mi - 1
+                    else:
+                        lo = mi + 1
+            return res
+        return [bin_s(nums, True), bin_s(nums, False)]
