fe56bda5e38a438f607ea296880b92a7fdd33503
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-04 13:11:30 +0300

    Time: 239 ms (14.77%) | Memory: 22.2 MB (5.56%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,22 +1,25 @@
+from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        if not mat or not mat[0]: return []
-        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        if not mat or not mat[0]: return mat
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         rows, cols = len(mat), len(mat[0])
-        res = [[-1] * cols for _ in range(rows)]
+        res = [[0] * cols for _ in range(rows)]
         q = deque()
+        sr = sc = None
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c] == 0:
                     q.append((r, c))
-                    res[r][c] = 0
+        visit = set()
+        visit.add((sr, sc))
         while q:
-            for _ in range(len(q)):
-                r, c = q.popleft()
-                for dr, dc in dirs:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < rows and 0 <= nc < cols and res[nr][nc] == -1:
+            r, c = q.popleft()
+            for dr, dc in dirs:
+                nr, nc = dr + r, dc + c
+                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visit:
+                    visit.add((nr, nc))
+                    if mat[nr][nc] == 1:
                         res[nr][nc] = res[r][c] + 1
-                        q.append((nr, nc))
+                    q.append((nr, nc))
         return res
-                    
