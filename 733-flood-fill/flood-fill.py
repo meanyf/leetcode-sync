b8b0c3e8002bba4a884e92125a369c5f88bf1d81
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-04 10:58:33 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (70.09%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,20 +1,18 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if not image:
-            return []
+        if not image or not image[0]: return
         rows, cols = len(image), len(image[0])
-        stack = [(sr, sc)]
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        original_color = image[sr][sc]
-        if original_color == color:
-            return image
+        orig = image[sr][sc]
+        stack = [(sr, sc)]
+        visit = set((sr, sc))
+        image[sr][sc] = color
         while stack:
-            i, j = stack.pop()
-            for di, dj in dirs:
-                ni, nj = di + i, dj + j
-                if ni < 0 or ni >= rows or nj < 0 or nj >= cols:
-                    continue
-                if original_color == image[ni][nj]:
-                    stack.append((ni, nj))
-            image[i][j] = color
+            r, c = stack.pop()
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and image[nr][nc] == orig and (nr, nc) not in visit:
+                    image[nr][nc] = color
+                    stack.append((nr, nc))
+                    visit.add((nr, nc))
         return image
