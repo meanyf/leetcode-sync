1e5e2df3d13d22870c1721933479a90d0c77c87a
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-04 09:32:59 +0300

    Time: 255 ms (44.36%) | Memory: 25.3 MB (26.30%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -8,16 +8,16 @@ class Solution:
             stack = [(i, j)]
             while stack:
                 i, j = stack.pop()
+                visited.add((i, j))
                 for di, dj in dirs:
                     ni, nj = di + i, dj + j
                     if 0 <= ni < rows and 0 <= nj < cols:
-                        if grid[i][j] == '1' and (ni, nj) not in visited:
+                        if grid[ni][nj] == '1' and (ni, nj) not in visited:
                             stack.append((ni, nj))
                             visited.add((ni, nj))
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == '1' and (i, j) not in visited:
                     dfs(i, j)
-                    visited.add((i, j))
                     res += 1
         return res
