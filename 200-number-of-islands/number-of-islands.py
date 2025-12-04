6ebafbcc344072237123c6756c7f5fc289cbce8b
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-04 09:31:40 +0300

    Time: 320 ms (8.06%) | Memory: 32.5 MB (5.74%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,32 +1,23 @@
-from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]:
-            return []
-        rows, cols = len(grid), len(grid[0])
-        visited = [[False] * cols for _ in range(rows)]
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
         res = 0
-        def bfs(start_r: int, start_c: int) -> None:
-            q = deque()
-            q.append((start_r, start_c))
-            visited[start_r][start_c] = True
-            while q:
-                cur_r, cur_c = q.popleft()
-                for dr, dc in dirs:
-                    nr, nc = cur_r + dr, cur_c + dc
-                    if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
-                        continue
-                    if visited[nr][nc] or grid[nr][nc] == "0":
-                        continue
-                    visited[nr][nc] = True
-                    q.append((nr, nc))
-
-        
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1' and not visited[r][c]:
-                    bfs(r, c)
+        def dfs(i, j):
+            stack = [(i, j)]
+            while stack:
+                i, j = stack.pop()
+                for di, dj in dirs:
+                    ni, nj = di + i, dj + j
+                    if 0 <= ni < rows and 0 <= nj < cols:
+                        if grid[i][j] == '1' and (ni, nj) not in visited:
+                            stack.append((ni, nj))
+                            visited.add((ni, nj))
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    dfs(i, j)
+                    visited.add((i, j))
                     res += 1
         return res
-
