c3597f55b890be2d099db4387547ba4542810374
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-05 09:07:22 +0300

    Time: 138 ms (49.90%) | Memory: 58 MB (41.11%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,37 +1,30 @@
-#
-# @lc app=leetcode id=380 lang=python3
-#
-# [380] Insert Delete GetRandom O(1)
-#
-
-# @lc code=start
-import random
-
+from random import randint
 class RandomizedSet:
 
     def __init__(self):
-        self.d = dict()
+        self.d = {}
         self.lst = []
 
     def insert(self, val: int) -> bool:
-        if val in self.d:
-            return False
-        self.d[val] = len(self.lst)
+        if val in self.d: return False
+        self.d[val] = len(self.d)
         self.lst.append(val)
         return True
 
     def remove(self, val: int) -> bool:
         if val not in self.d:
             return False
-        id = self.d[val]
-        self.lst[id] = self.lst[-1]
-        self.d[self.lst[-1]] = id
+        i = self.d[val]
+        prev_last_val = self.lst[-1]
+        self.lst[i], self.lst[-1] = self.lst[-1], self.lst[i]
+        self.d[prev_last_val] = i
         self.lst.pop()
         del self.d[val]
         return True
 
     def getRandom(self) -> int:
-        return random.choice(self.lst)
+        r = randint(0, len(self.lst) - 1)
+        return self.lst[r]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
@@ -39,4 +32,3 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
-# @lc code=end
