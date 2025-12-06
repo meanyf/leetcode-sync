abfe74e79dac7be546e8bb2540df0ba32605df9c
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-06 11:36:14 +0300

    Time: 7 ms (66.98%) | Memory: 18.8 MB (99.08%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,42 +1,35 @@
-#
-# @lc app=leetcode id=658 lang=python3
-#
-# [658] Find K Closest Elements
-#
-
-# @lc code=start
-from collections import deque
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        lo = 0
-        hi = len(arr) - 1
-        while lo < hi:
-            mi = (lo + hi) // 2
-            if arr[mi] < x:
-                lo = mi + 1
+        l, r = 0, len(arr) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if arr[m] < x:
+                l = m + 1
             else:
-                hi = mi
-        if lo == len(arr) or (lo == 0 and arr[lo] == x):
-            if lo == 0:
-                return arr[:k]
-            if lo == len(arr):
-                return arr[-k:]
-        l = lo
-        if arr[lo] != x and abs(x - arr[lo - 1]) <= abs(x - arr[lo]):
-            l -= 1
+                r = m - 1
+        if l == len(arr): 
+            l = len(arr) - 1
+        res = abs(arr[l] - x)
+        ans = l
+        if l > 0 and res >= abs(arr[l - 1] - x):
+            ans = l - 1
+            res = abs(arr[l - 1] - x)
+        if l < len(arr) - 1 and res > abs(arr[l + 1] - x):
+            ans = l + 1
+        l = ans
         r = l + 1
+        print(l, r)
         cnt = 0
-        res = deque()
         while cnt < k:
-            left = arr[l] if l > -1 else float('inf')
-            right = arr[r] if r < len(arr) else float('inf')
-            if abs(x - left) <= abs(x - right):
-                res.appendleft(arr[l])
+            if r >= len(arr):
+                l -= 1
+            elif l < 0:
+                r += 1
+            elif abs(arr[l] - x) <= abs(arr[r] - x):
                 l -= 1
             else:
-                res.append(arr[r])
                 r += 1
             cnt += 1
-        return list(res)
-# @lc code=end
-
+        return arr[l + 1: r]
+            
+        
