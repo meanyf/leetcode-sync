3a8924e2840acc29f3fdb0de7e3b5275ca5143ca
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-07 13:55:01 +0300

    Time: 27 ms (67.80%) | Memory: 20.4 MB (21.96%) - LeetSync

--- 2352-design-an-atm-machine/design-an-atm-machine.py
+++ 2352-design-an-atm-machine/design-an-atm-machine.py
@@ -2,24 +2,23 @@ class ATM:
 
     def __init__(self):
         self.banknotes = [500, 200, 100, 50, 20]
-        self.counts = {banknote:0 for banknote in self.banknotes}
+        self.counts = [0] * 5
 
     def deposit(self, banknotesCount: List[int]) -> None:
-        for cnt, banknote in zip(banknotesCount[::-1], self.banknotes):
-            self.counts[banknote] += cnt
+        for i, cnt in enumerate(banknotesCount[::-1]):
+            self.counts[i] += cnt
 
     def withdraw(self, amount: int) -> List[int]:
         res = []
-        original = self.counts.copy()
-        for banknote in self.banknotes:
-            total = min(amount // banknote, self.counts[banknote])
+        for i, banknote in enumerate(self.banknotes):
+            total = min(amount // banknote, self.counts[i])
             amount -= total * banknote
-            self.counts[banknote] -= total
             res.append(total)
         if amount == 0:
+            for i in range(len(res)):
+                self.counts[i] -= res[i]
             return res[::-1]
         else:
-            self.counts = original
             return [-1]
 
 # Your ATM object will be instantiated and called as such:
