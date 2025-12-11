3bb8c0e47431d5b9e90e40055d47136384523d77
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-11 17:17:43 +0300

    Time: 46 ms (40.41%) | Memory: 18.1 MB (59.03%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -10,27 +10,27 @@ class Solution:
             stack = [(a, b)]
             while stack:
                 x, y = stack.pop()
-                if not x and not y:
+                if x is None and y is None:
                     continue
-                if not x or not y or x.val != y.val:
+                if x is None or y is None or x.val != y.val:
                     return False
                 stack.append((x.right, y.right))
                 stack.append((x.left, y.left))
             return True
 
-        if not subRoot:
+        if subRoot is None:
             return True
         
-        if not root:
+        if root is None:
             return False
             
         stack = [root]
         while stack:
             node = stack.pop()
             if node:
-                stack.append(node.right)
-                stack.append(node.left)
                 if node.val == subRoot.val:
                     if check(node, subRoot):
                         return True
+                stack.append(node.right)
+                stack.append(node.left)
         return False
