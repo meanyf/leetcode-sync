b330c68bd43fa2d2afe07aab20eb1d18dd1472dc
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-11 16:44:14 +0300

    Time: 5 ms (35.87%) | Memory: 19.1 MB (6.92%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -1,10 +1,3 @@
-#
-# @lc app=leetcode id=110 lang=python3
-#
-# [110] Balanced Binary Tree
-#
-
-# @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -15,47 +8,17 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
-        stack = [(root, False, 0)]
         d = {}
+        stack = [(root, False)]
         while stack:
-            node, visited, depth = stack.pop()
+            node, visited = stack.pop()
             if node:
                 if visited:
-                    if not node.left and not node.right:
-                        d[node] = 0
-                    else:
-                        d[node] = max(d.get(node.left, 0), d.get(node.right, 0)) + 1
-                    if node.left and node.right:
-                        if abs(d[node.left] - d[node.right]) > 1:
-                            return False
-                    if node.left and not node.right:
-                        if d[node] > 1:
-                            return False
-                    if node.right and not node.left:
-                        # print(d[node.right])
-                        if d[node] > 1:
-                            return False
+                    if abs(d.get(node.left, 0) - d.get(node.right, 0)) > 1:
+                        return False
+                    d[node] = max(d.get(node.left, 0), d.get(node.right, 0)) + 1
                 else:
-                    stack.append((node, True, depth))
-
-                # if node.right:
-                if node.right:
-                    stack.append((node.right, False, depth + 1))
-                if node.left:
-                # if node.left:
-                    stack.append((node.left, False, depth + 1))
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
         return True
-        # x = True
-        # def dfs(node):
-        #     nonlocal x
-        #     if not node:
-        #         return 0
-        #     left_depth = dfs(node.left)
-        #     right_depth = dfs(node.right)
-        #     if abs(left_depth - right_depth) > 1:
-        #         x = False
-        #     return 1 + max(left_depth, right_depth)
-        # dfs(root)
-        # return x
-# @lc code=end
-
