0ac4b663f593fd9d11691d6e102cf969b1e3557c
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-12 10:03:08 +0300

    Time: 64 ms (8.96%) | Memory: 18.9 MB (81.16%) - LeetSync

--- 652-find-duplicate-subtrees/find-duplicate-subtrees.py
+++ 652-find-duplicate-subtrees/find-duplicate-subtrees.py
@@ -1,10 +1,3 @@
-#
-# @lc app=leetcode id=652 lang=python3
-#
-# [652] Find Duplicate Subtrees
-#
-
-# @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -14,25 +7,24 @@
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
         d = {}
+        d2 = {}
+        if not root:
+            return []
         stack = [(root, False)]
+        res = []
         while stack:
             node, visited = stack.pop()
             if node:
                 if visited:
-                    left = d.get(node.left, "#")
-                    right = d.get(node.right, "#")
-                    serial = f'{node.val},{left},{right}'
-                    d[node] = serial
+                    left_ser = d.get(node.left, 'None')
+                    right_ser = d.get(node.right, 'None')
+                    val = (left_ser, node.val, right_ser)
+                    d[node] = val
+                    d2[val] = d2.get(val, 0) + 1
+                    if d2[val] == 2:
+                        res.append(node)
                 else:
                     stack.append((node, True))
                     stack.append((node.right, False))
                     stack.append((node.left, False))
-        g = {}
-        res = []
-        for key, val in d.items():
-            g[val] = g.get(val, 0) + 1
-            if g[val] == 2:
-                res.append(key)
         return res
-
-# @lc code=end
