e7a9b4756972abcd8a7abe9f5937f77863c88d54
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-12 18:44:26 +0300

    Time: 15 ms (9.65%) | Memory: 21.7 MB (33.04%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,54 +1,19 @@
-#
-# @lc app=leetcode id=56 lang=python3
-#
-# [56] Merge Intervals
-#
-
-# @lc code=start
 class Solution:
-    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
-        intervals.sort(key=lambda x: x[0])
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        i = 0
         res = []
-        if not intervals:
-            return []
-        current = intervals[0]
-        for i in range(1, len(intervals)):
-            item = intervals[i]
-            if item[0] <= current[1] <= item[1] or current[0] <= item[0] <= current[1]:
-                current[0] = min(current[0], item[0])
-                current[1] = max(current[1], item[1])
+        l, r = intervals[0][0], intervals[0][1]
+        while i < len(intervals):
+            prev_l, prev_r = l, r
+            l = max(l, intervals[i][0])
+            r = min(r, intervals[i][1])
+            if l > r:
+                res.append([prev_l, prev_r])
+                l, r = intervals[i][0], intervals[i][1]
             else:
-                res.append(current)
-                current = item
-        if current:
-            res.append(current)
+                l = min(prev_l, intervals[i][0])
+                r = max(prev_r, intervals[i][1])
+            i += 1
+        res.append([l, r])
         return res
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # lst = []
-        # length = len(intervals)
-        # # for i in range(len(intervals)):
-
-        # left = 0
-        # res = []
-        # while left < length - 1:
-        #     if intervals[left + 1][0] <= intervals[left][1] <= intervals[left + 1][1]:
-        #         res = [intervals[left + 1][0], intervals[left + 1][1]]
-        #     else:
-        #         if res:
-        #             lst.append(res)
-                
-# @lc code=end
-
