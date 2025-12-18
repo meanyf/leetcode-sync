0cc12c682637bbf35b58255c5216e9bd76378ff3
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-18 12:46:53 +0300

    Time: 0 ms (100.00%) | Memory: 17.4 MB (99.98%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,37 +1,21 @@
-#
-# @lc app=leetcode id=443 lang=python3
-#
-# [443] String Compression
-#
-
-# @lc code=start
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        if not chars:
-            return 0
-        prev = chars[0]
-        cnt = 1
-        insert = 0
-        for i in range(1, len(chars)):
-            if chars[i] == prev:
+        cnt = 0
+        l = r = 0
+        res = 0
+        while l < len(chars) and r < len(chars):
+            res += 1
+            cnt = 0
+            cur = chars[r]
+            while r < len(chars) and cur == chars[r]:
+                r += 1
                 cnt += 1
-            else:
-                chars[insert] = prev
-                insert += 1
-                if cnt > 1:
-                    cnt_list = list(str(cnt))
-                    for ch in cnt_list:
-                        chars[insert] = ch
-                        insert += 1
-                cnt = 1
-            prev = chars[i]
-        chars[insert] = prev
-        insert += 1
-        if cnt > 1:
-            cnt_list = list(str(cnt))
-            for ch in cnt_list:
-                chars[insert] = ch
-                insert += 1
-        return insert
-# @lc code=end
-
+            chars[l] = cur
+            l += 1
+            if cnt != 1:
+                size = len(str(cnt))
+                for ch in str(cnt):
+                    chars[l] = ch
+                    res += 1
+                    l += 1
+        return res
