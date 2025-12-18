207b6db47873a1ddec35c96ed339440bfe069059
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-18 10:45:40 +0300

    Time: 83 ms (15.10%) | Memory: 21.8 MB (9.44%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,24 +1,10 @@
-#
-# @lc app=leetcode id=1493 lang=python3
-#
-# [1493] Longest Subarray of 1's After Deleting One Element
-#
-
-# @lc code=start
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        l = 0
-        cnt_zero = 0
-        mx = 0
-        for r in range(len(nums)):
-            if nums[r] == 0:
-                cnt_zero += 1
-            while cnt_zero > 1:
-                if nums[l] == 0:
-                    cnt_zero -= 1
+        cnt = res = l = 0
+        for r, item in enumerate(nums):
+            cnt += 1 if item == 0 else 0
+            while cnt > 1:
+                cnt -= 1 if nums[l] == 0 else 0
                 l += 1
-            mx = max(mx, r - l)
-        return mx
-            
-# @lc code=end
-
+            res = max(res, r - l)
+        return res
