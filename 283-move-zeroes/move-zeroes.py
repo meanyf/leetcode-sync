b32516ebc476bbb0413e59d1d88097f4b8baf57a
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-19 11:24:58 +0300

    Time: 7 ms (42.63%) | Memory: 18.5 MB (99.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,8 +6,5 @@ class Solution:
         l = 0
         for r in range(len(nums)):
             if nums[r] != 0:
-                nums[l] = nums[r]
+                nums[l], nums[r] = nums[r], nums[l]
                 l += 1
-
-        for l in range(l, len(nums)):
-            nums[l] = 0
