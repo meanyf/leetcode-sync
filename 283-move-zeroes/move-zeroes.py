7c7f36109496f6ef10c764d74f2d78feaa823bc5
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-19 10:49:35 +0300

    Time: 4 ms (59.65%) | Memory: 18.8 MB (49.64%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,13 +6,11 @@ class Solution:
         l = 0
         cnt = 0
         for r in range(len(nums)):
-            while l < len(nums) and nums[l] == 0:
-                cnt += 1
+            if nums[r] != 0:
+                nums[l] = nums[r]
                 l += 1
-            if l < len(nums):
-               nums[r] = nums[l]
-               l += 1
-        
+            else:
+                cnt += 1
+
         for i in range(1, cnt + 1):
             nums[-i] = 0
-        
