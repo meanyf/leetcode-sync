e0e6f3265530b63fec1948653c9dd358052bcd02
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-19 17:17:07 +0300

    Time: 7 ms (39.54%) | Memory: 17.1 MB (100.00%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -3,14 +3,13 @@ class Solution:
         d = {}
         for i, item in enumerate(s):
             d[item] = i
-        
-        mx = -1
-        res = []
-        l = 0
+            
+        res =[]
+        mx = l = 0
         for i, ch in enumerate(s):
             mx = max(mx, d[ch])
             if i == mx:
-                mx = -1
+                mx = 0
                 res.append(i - l + 1)
                 l = i + 1
         return res
