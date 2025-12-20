d5370c672ab2742a6118728ab9bde306e0f294c3
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-20 11:04:10 +0300

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.76%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -5,27 +5,20 @@ class MyQueue:
         self.stack2 = []
 
     def push(self, x: int) -> None:
-        if not self.stack2:
-            self.stack1.append(x)
-        else:
-            self.stack2.append(x)
+        self.stack1.append(x)
 
     def pop(self) -> int:
         if not self.stack2:
             ans = self.stack1[0]
             for i in range(1, len(self.stack1)):
-                self.stack2.append(self.stack1[i])
+                self.stack2.append(self.stack1[-i])
             self.stack1.clear()
             return ans
         else:
-            ans = self.stack2[0]
-            for i in range(1, len(self.stack2)):
-                self.stack1.append(self.stack2[i])
-            self.stack2.clear()
-            return ans
+            return self.stack2.pop()
 
     def peek(self) -> int:
-        return self.stack1[0] if self.stack1 else self.stack2[0]
+        return self.stack2[-1] if self.stack2 else self.stack1[0] 
 
     def empty(self) -> bool:
         return True if not self.stack1 and not self.stack2 else False
