ed94d23bf6b242410e5b4b35d03fb6b57fe2c3a7
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-21 11:09:08 +0300

    Time: 4 ms (67.79%) | Memory: 20.7 MB (99.06%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,12 @@
-#
-# @lc app=leetcode id=347 lang=python3
-#
-# [347] Top K Frequent Elements
-#
-
-# @lc code=start
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d = {}
         for item in nums:
             d[item] = d.get(item, 0) + 1
-        lst = list(d.items())
-        lst.sort(key = lambda x: -x[1])
-        return [x[0] for x in lst[:k]]
-# @lc code=end
-
+        
+        res = [(key, val) for key, val in d.items()]
+        res.sort(key=lambda x: -x[1])
+        ans = []
+        for i in range(k):
+            ans.append(res[i][0])
+        return ans
