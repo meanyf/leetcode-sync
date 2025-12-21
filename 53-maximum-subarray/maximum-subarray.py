57f71cd370ba6b93291f69a8bbf30541bfd3e389
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-21 13:37:07 +0300

    Time: 85 ms (43.80%) | Memory: 29.9 MB (99.10%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,24 +1,8 @@
-#
-# @lc app=leetcode id=53 lang=python3
-#
-# [53] Maximum Subarray
-#
-
-
-# @lc code=start
-from functools import cache
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        bst = float('-inf')
-        def run(i, cur):
-            nonlocal bst
-            bst = max(bst, cur)
-            if i == n: 
-                return cur
-            if cur + nums[i] >= nums[i]:
-                return run(i + 1, cur + nums[i])
-            return run(i + 1, nums[i])
-        run(0, float('-inf'))
-        return bst
-# @lc code=end
+        cur = 0
+        best = -math.inf
+        for item in nums:
+            cur = max(item, cur + item)
+            best = max(best, cur)
+        return best
