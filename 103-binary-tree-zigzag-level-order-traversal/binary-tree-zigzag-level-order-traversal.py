34c512c63dc14d60e450c34bc742903a9045d32c
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-21 18:32:45 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (98.02%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -1,39 +1,28 @@
-#
-# @lc app=leetcode id=103 lang=python3
-#
-# [103] Binary Tree Zigzag Level Order Traversal
-#
-
-# @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        queue = deque([root])
+        q = deque([root])
         res = []
-        x = True
-        while queue:
+        right = True
+        while q:
             lst = []
-            level = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level.append(node.val)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            if x:
-                res.append(level)
-            else:
-                level.reverse()
-                res.append(level)
-            x = not x
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node:
+                    lst.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if lst:
+                if right:
+                    res.append(lst)
+                else:
+                    res.append(lst[::-1])
+            right = not right
         return res
-# @lc code=end
