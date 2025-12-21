d5d0bab4561d7ca44ab30e68fb171c13c19e6152
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-21 10:58:23 +0300

    Time: 7 ms (76.86%) | Memory: 18.1 MB (98.30%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.py
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.py
@@ -4,10 +4,9 @@ class Solution:
         while l < len(seats) and seats[l] == 0:
             l += 1
         res = r = l
-        while r < len(seats):
+        for r in range(r, len(seats)):
             if seats[r] == 1:
                 res = max(res, (r - l) // 2)
                 l = r
-            r += 1
         res = max(res, abs(len(seats) - 1 - l))
         return res
