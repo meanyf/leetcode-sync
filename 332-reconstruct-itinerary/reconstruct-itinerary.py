c50837510830a87876a061be80f8bb80c1cba3a7
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-22 09:26:44 +0300

    Time: 0 ms (100.00%) | Memory: 17.4 MB (99.95%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -2,15 +2,16 @@ from collections import defaultdict
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         g = defaultdict(list)
-        tickets.sort()
         for src, dest in tickets:
             g[src].append(dest)
+        for src in g:
+            g[src].sort(reverse=True)
         stack = ['JFK']
         res = []
         while stack:
             node = stack[-1]
             if g[node]:
-                next_node = g[node].pop(0)
+                next_node = g[node].pop()
                 stack.append(next_node)
             else:
                 res.append(stack.pop())
