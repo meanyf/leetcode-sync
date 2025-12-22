be5536385dbef67b1b5f341be2c18679c5564b59
meanyf <112635638+meanyf@users.noreply.github.com>
2025-12-22 09:42:26 +0300

    Time: 270 ms (27.19%) | Memory: 24.9 MB (32.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]: return 0
+        if not grid or not grid[0]:
+            return 0
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        rows, cols = len(grid), len(grid[0])
         visited = set()
-        res = 0
+
         def dfs(i, j):
-            stack = [(i, j)]
             visited.add((i, j))
+            stack = [(i, j)]
             while stack:
                 i, j = stack.pop()
                 for di, dj in dirs:
                     ni, nj = di + i, dj + j
-                    if 0 <= ni < rows and 0 <= nj < cols:
+                    if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]):
                         if grid[ni][nj] == '1' and (ni, nj) not in visited:
-                            stack.append((ni, nj))
                             visited.add((ni, nj))
+                            stack.append((ni, nj))
 
-        for i in range(rows):
-            for j in range(cols):
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == '1' and (i, j) not in visited:
                     dfs(i, j)
                     res += 1
